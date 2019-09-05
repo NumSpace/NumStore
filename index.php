d6ad003b50c7618fc8ff255ed072ec428cebfd53
numspace <abdoul_gandega@hotmail.fr>
2019-09-05 20:22:35 +0200

    jquery test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+ 
+</body>
+</html>
